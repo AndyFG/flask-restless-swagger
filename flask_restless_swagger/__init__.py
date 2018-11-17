271d2cbbe78b5d5d4f0bc6277f34ad8a8e9e6e2f
Andy Fernandez <afgarabote@gmail.com>
2018-11-17 18:52:41 -0500

    remove unneeded files

--- flask_restless_swagger/__init__.py
+++ flask_restless_swagger/__init__.py
@@ -45,8 +45,6 @@ def get_columns(model):
 class SwagAPIManager(object):
     swagger = {
         'swagger': '2.0',
-        'info': {},
-        'schemes': ['http'],
         'basePath': '/api/',
         'consumes': ['application/vnd.api+json'],
         'produces': ['application/vnd.api+json'],
@@ -61,68 +59,26 @@ class SwagAPIManager(object):
         if app is not None:
             self.init_app(app, **kwargs)
 
-    def to_json(self, **kwargs):
-        return json.dumps(self.swagger, **kwargs)
-
-    def to_yaml(self, **kwargs):
-        return yaml.dump(self.swagger, **kwargs)
-
-    def __str__(self):
-        return self.to_json(indent=4)
-
-    @property
-    def version(self):
-        if 'version' in self.swagger['info']:
-            return self.swagger['info']['version']
-        return None
-
-    @version.setter
-    def version(self, value):
-        self.swagger['info']['version'] = value
-
-    @property
-    def title(self):
-        if 'title' in self.swagger['info']:
-            return self.swagger['info']['title']
-        return None
-
-    @title.setter
-    def title(self, value):
-        self.swagger['info']['title'] = value
-
-    @property
-    def description(self):
-        if 'description' in self.swagger['info']:
-            return self.swagger['info']['description']
-        return None
-
-    @description.setter
-    def description(self, value):
-        self.swagger['info']['description'] = value
-
     def add_path(self, model, **kwargs):
         name = model.__tablename__
         schema = model.__name__
-        path = kwargs.get('url_prefix', "") + '/' + name.lower()
+        path = kwargs.get('url_prefix', "") + '/' + name
         id_path = "{0}/{{{1}Id}}".format(path, schema.lower())
         self.swagger['paths'][path] = {}
 
         for method in [m.lower() for m in kwargs.get('methods', ['GET'])]:
             if method == 'get':
                 self.swagger['paths'][path][method] = {
-                    'parameters': [{
-                        'name': 'q',
-                        'in': 'query',
-                        'description': 'searchjson',
-                        'type': 'string'
-                    }],
+                    'parameters': [
+                        {'name': 'q', 'in': 'query', 'description': 'Resource parameters', 'type': 'string'}
+                    ],
                     'responses': {
                         200: {
-                            'description': 'List ' + name.lower(),
+                            'description': 'List ' + name.capitalize(),
                             'schema': {
-                                'title': name.lower(),
+                                'title': name,
                                 'type': 'array',
-                                'items': {'$ref': '#/definitions/' + name.lower()}
+                                'items': {'$ref': '#/definitions/' + name.capitalize()}
                             }
                         }
                     }
@@ -136,16 +92,16 @@ class SwagAPIManager(object):
 
                 self.swagger['paths'][id_path][method] = {
                     'parameters': [{
-                        'name': schema.lower() + 'Id',
+                        'name': schema + 'Id',
                         'in': 'path',
-                        'description': 'ID of ' + schema.lower(),
+                        'description': 'ID of ' + schema,
                         'required': True,
                         'type': 'integer'
                     }],
                     'responses': {
                         200: {
-                            'description': 'Success ' + name.lower(),
-                            'schema': {'title': name.lower(), '$ref': '#/definitions/' + name.lower()}
+                            'description': 'Success ' + name.capitalize(),
+                            'schema': {'title': name, '$ref': '#/definitions/' + name.capitalize()}
                         }
                     }
                 }
@@ -174,7 +130,7 @@ class SwagAPIManager(object):
                         'name': name.lower(),
                         'in': 'body',
                         'description': schema,
-                        'type': "#/definitions/" + schema.lower()
+                        'type': "#/definitions/" + schema
                     }],
                     'responses': {200: {'description': 'Success'}}
                 }
@@ -183,7 +139,7 @@ class SwagAPIManager(object):
 
     def add_defn(self, model, **kwargs):
         name = model.__name__
-        self.swagger['definitions'][name.lower()] = {'type': 'object', 'properties': {}}
+        self.swagger['definitions'][name] = {'type': 'object', 'properties': {}}
         columns = get_columns(model).keys()
 
         for column_name, column in get_columns(model).items():
@@ -207,7 +163,7 @@ class SwagAPIManager(object):
             if column.__doc__:
                 column_defn['description'] = column.__doc__
 
-            self.swagger['definitions'][name.lower()]['properties'][column_name] = column_defn
+            self.swagger['definitions'][name]['properties'][column_name] = column_defn
 
     def init_app(self, app, **kwargs):
         self.app = app
