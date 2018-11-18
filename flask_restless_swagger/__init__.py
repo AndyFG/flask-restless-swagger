400f37f85d3ddb08f57954bca5492f4509080d7a
Andy Fernandez <afgarabote@gmail.com>
2018-11-18 12:46:50 -0500

    fix swagger json generation for Flask 0.12.2

--- flask_restless_swagger/__init__.py
+++ flask_restless_swagger/__init__.py
@@ -1,4 +1,6 @@
 import json
+from uuid import uuid4
+
 import yaml
 from urllib import parse as urlparse
 from flask import jsonify, request, Blueprint
@@ -7,25 +9,25 @@ from sqlalchemy.orm.attributes import InstrumentedAttribute
 from flask_restless.helpers import *
 
 sqlalchemy_swagger_type = {
-    'INTEGER': 'integer',
-    'SMALLINT': 'int32',
-    'NUMERIC': 'number',
-    'DECIMAL': 'number',
-    'VARCHAR': 'string',
-    'TEXT': 'string',
-    'DATE': 'date',
-    'TIME': 'date',
-    'BOOLEAN': 'bool',
-    'BLOB': 'binary',
-    'BYTEA': 'binary',
-    'BINARY': 'binary',
-    'VARBINARY': 'binary',
-    'FLOAT': 'float',
-    'REAL': 'float',
-    'DATETIME': 'date-time',
-    'BIGINT': 'int64',
-    'ENUM': 'string',
-    'INTERVAL': 'date-time',
+    'INTEGER': {'type': 'integer', 'format': 'int64'},
+    'SMALLINT': {'type': 'int32', 'format': 'int32'},
+    'NUMERIC': {'type': 'number', 'format': 'int64'},
+    'DECIMAL': {'type': 'number', 'format': 'int64'},
+    'VARCHAR': {'type': 'string', 'example': 'lorem ipsum...'},
+    'TEXT': {'type': 'string', 'example': 'lorem ipsum...'},
+    'DATE': {'type': 'date', 'example': '03/11/1988'},
+    'TIME': {'type': 'date', 'example': '03/11/1988'},
+    'BOOLEAN': {'type': 'boolean', 'example': 'true'},
+    'BLOB': {'type': 'binary', 'example': 'Binary data'},
+    'BYTEA': {'type': 'binary', 'example': 'Binary data'},
+    'BINARY': {'type': 'binary', 'example': 'Binary data'},
+    'VARBINARY': {'type': 'binary', 'example': 'Binary data'},
+    'FLOAT': {'type': 'float', 'example': '3.14'},
+    'REAL': {'type': 'float', 'example': '3.14'},
+    'DATETIME': {'type': 'date', 'example': '03/11/1988'},
+    'BIGINT': {'type': 'int64', 'format': 'int64'},
+    'ENUM': {'type': 'string', 'example': 'Enum content type'},
+    'INTERVAL': {'type': 'date', 'example': '03/11/1988'}
 }
 
 
@@ -45,9 +47,11 @@ def get_columns(model):
 class SwagAPIManager(object):
     swagger = {
         'swagger': '2.0',
-        'basePath': '/api/',
-        'consumes': ['application/vnd.api+json'],
-        'produces': ['application/vnd.api+json'],
+        'info': {},
+        'schemes': ['http', 'https'],
+        'basePath': '/api',
+        'consumes': ['application/json'],
+        'produces': ['application/json'],
         'paths': {},
         'definitions': {}
     }
@@ -56,6 +60,9 @@ class SwagAPIManager(object):
         self.app = None
         self.manager = None
 
+        if 'swagger' in kwargs:
+            self.swagger.update(kwargs.pop('swagger'))
+
         if app is not None:
             self.init_app(app, **kwargs)
 
@@ -63,79 +70,99 @@ class SwagAPIManager(object):
         name = model.__tablename__
         schema = model.__name__
         path = kwargs.get('url_prefix', "") + '/' + name
-        id_path = "{0}/{{{1}Id}}".format(path, schema.lower())
+        id_path = "{0}/{{{1}-id}}".format(path, schema.lower())
+        model_description = model.__doc__ if model.__doc__ else ''
+
         self.swagger['paths'][path] = {}
 
-        for method in [m.lower() for m in kwargs.get('methods', ['GET'])]:
-            if method == 'get':
-                self.swagger['paths'][path][method] = {
-                    'parameters': [
-                        {'name': 'q', 'in': 'query', 'description': 'Resource parameters', 'type': 'string'}
-                    ],
-                    'responses': {
-                        200: {
-                            'description': 'List ' + name.capitalize(),
-                            'schema': {
-                                'title': name,
-                                'type': 'array',
-                                'items': {'$ref': '#/definitions/' + name.capitalize()}
+        for method in [m.lower() for m in ['GET', 'POST', 'DELETE', 'PATCH']]:
+            if name.capitalize() in self.swagger['definitions']:
+                if method == 'get':
+                    self.swagger['paths'][path][method] = {
+                        'tags': [name.capitalize()],
+                        'summary': 'List of %ss' % name,
+                        'description': model_description,
+                        'parameters': [
+                            {'name': 'q', 'in': 'query', 'description': 'Resource params', 'type': 'string'}],
+                        'responses': {
+                            200: {
+                                'description': 'List of %ss' % name,
+                                'schema': {
+                                    'title': name,
+                                    'type': 'array',
+                                    'items': {'$ref': '#/definitions/' + name.capitalize()}
+                                }
                             }
                         }
                     }
-                }
 
-                if model.__doc__:
-                    self.swagger['paths'][path]['description'] = model.__doc__
+                    if id_path not in self.swagger['paths']:
+                        self.swagger['paths'][id_path] = {}
 
-                if id_path not in self.swagger['paths']:
-                    self.swagger['paths'][id_path] = {}
-
-                self.swagger['paths'][id_path][method] = {
-                    'parameters': [{
-                        'name': schema + 'Id',
-                        'in': 'path',
-                        'description': 'ID of ' + schema,
-                        'required': True,
-                        'type': 'integer'
-                    }],
-                    'responses': {
-                        200: {
-                            'description': 'Success ' + name.capitalize(),
-                            'schema': {'title': name, '$ref': '#/definitions/' + name.capitalize()}
+                    self.swagger['paths'][id_path][method] = {
+                        'tags': [name.capitalize()],
+                        'summary': 'List one ' + name,
+                        'description': model_description,
+                        'parameters': [{
+                            'name': schema.lower() + '_id',
+                            'in': 'path',
+                            'description': 'ID of ' + schema,
+                            'required': True,
+                            'type': 'integer',
+                            'example': uuid4()
+                        }],
+                        'responses': {
+                            200: {
+                                'description': 'Success ' + name,
+                                'schema': {
+                                    'title': name,
+                                    '$ref': '#/definitions/' + name.capitalize()
+                                }
+                            }
                         }
                     }
-                }
-                if model.__doc__:
-                    self.swagger['paths'][id_path]['description'] = model.__doc__
 
-            elif method == 'delete':
-                if id_path not in self.swagger['paths']:
-                    self.swagger['paths'][id_path] = {}
+                elif method == 'delete':
+                    if id_path not in self.swagger['paths']:
+                        self.swagger['paths'][id_path] = {}
 
-                self.swagger['paths']["{0}/{{{1}Id}}".format(path, schema.lower())][method] = {
-                    'parameters': [{
-                        'name': schema.lower() + 'Id',
-                        'in': 'path',
-                        'description': 'ID of ' + schema,
-                        'required': True,
-                        'type': 'integer'
-                    }],
-                    'responses': {200: {'description': 'Success'}}
-                }
-                if model.__doc__:
-                    self.swagger['paths'][id_path]['description'] = model.__doc__
-            else:
-                self.swagger['paths'][path][method] = {
-                    'parameters': [{
-                        'name': name.lower(),
-                        'in': 'body',
-                        'description': schema,
-                        'type': "#/definitions/" + schema
-                    }],
-                    'responses': {200: {'description': 'Success'}}
-                }
-                if model.__doc__:
-                    self.swagger['paths'][path]['description'] = model.__doc__
+                    self.swagger['paths']["{0}/{{{1}-id}}".format(path, schema.lower())][method] = {
+                        'tags': [name.capitalize()],
+                        'summary': 'Delete ' + name,
+                        'description': model_description,
+                        'parameters': [{
+                            'name': schema + '_id',
+                            'in': 'path',
+                            'description': 'ID of ' + schema,
+                            'required': True,
+                            'type': 'integer'
+                        }],
+                        'responses': {
+                            200: {
+                                'description': 'Success'
+                            }
+                        }
+                    }
+
+                else:
+                    response = {200: {'description': 'Success'}}
+                    if method == 'post':
+                        response.get(200).update(
+                            {'schema': {'title': name, '$ref': '#/definitions/' + name.capitalize()}}
+                        )
+
+                    self.swagger['paths'][path][method] = {
+                        'tags': [name.capitalize()],
+                        'summary': '%s %s' % ('Create' if method == 'post' else 'Update', name),
+                        'description': model_description,
+                        'parameters': [{
+                            'name': name.lower(),
+                            'in': 'body',
+                            'description': schema,
+                            'schema': {'$ref': "#/definitions/" + schema}
+                        }],
+                        'responses': response
+                    }
 
     def add_defn(self, model, **kwargs):
         name = model.__name__
@@ -143,27 +170,23 @@ class SwagAPIManager(object):
         columns = get_columns(model).keys()
 
         for column_name, column in get_columns(model).items():
-            if column_name in kwargs.get('exclude_columns', []):
-                continue
-            try:
+            column_defn = None
 
-                column_type = str(column.type)
-                if '(' in column_type:
-                    column_type = column_type.split('(')[0]
-                column_defn = sqlalchemy_swagger_type[column_type]
+            if column_name not in kwargs.get('exclude_columns', []):
+                try:
+                    column_type = str(column.type) if '(' not in str(column.type) else str(column.type).split('(')[0]
+                    column_defn = sqlalchemy_swagger_type[column_type]
+                except AttributeError:
+                    schema = get_related_model(model, column_name)
 
-            except AttributeError:
-                schema = get_related_model(model, column_name)
+                    if schema.__name__ in self.swagger['definitions']:
+                        if column_name + '_id' in columns:
+                            column_defn = {'schema': {'type': 'uuid', '$ref': schema.__name__}}
+                        else:
+                            column_defn = {'schema': {'type': 'array', 'items': {'$ref': schema.__name__}}}
 
-                if column_name + '_id' in columns:
-                    column_defn = {'schema': {'$ref': schema.__name__.lower()}}
-                else:
-                    column_defn = {'schema': {'type': 'array', 'items': {'$ref': schema.__name__.lower()}}}
-
-            if column.__doc__:
-                column_defn['description'] = column.__doc__
-
-            self.swagger['definitions'][name]['properties'][column_name] = column_defn
+            if column_defn:
+                self.swagger['definitions'][name]['properties'][column_name] = column_defn
 
     def init_app(self, app, **kwargs):
         self.app = app
